8db50d325e58cb908813b67233180527b453763e
shiva508 <dasarishiva1@gmail.com>
2023-07-30 19:46:29 +0530

    form changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('f') signUpForm!: NgForm;
+  genders: string[] = ['Male', 'female'];
   title = 'angular-zero-to-hero';
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+  constructor() {}
+  suggestedName() {
+    this.signUpForm.setValue({
+      userData: { username: 'Shiva' },
+    });
+  }
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
 }
